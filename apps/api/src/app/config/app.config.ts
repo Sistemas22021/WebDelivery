a01b160f9be3959bb1cf93f2084f8517ea31e0c6
Sentineldev <figueraj196@gmail.com>
2023-07-01 12:53:19 -0400

    Agregada archivo de configuracion .env y agregada funcionalidad para leer las variables de este archivo

---
+++ apps/api/src/app/config/app.config.ts
@@ -0,0 +1,20 @@
+
+
+interface ServerConfig {
+    port: number;
+    env: string;
+    prefix: string;
+
+}
+interface AppConfig {
+    server: ServerConfig
+}
+
+export default (): AppConfig => ({
+
+    server: {
+        port: parseInt(process.env.APP_PORT) || 3000,
+        env: process.env.APP_ENV || 'development',
+        prefix: process.env.APP_PREFIX || 'api',
+    }
+})
--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -7,19 +7,26 @@ import { HttpStatus, Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app/app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService);
+  
+  const globalPrefix = configService.get('server.prefix');
+  const port = configService.get('server.port');
+
 
   app.useGlobalPipes(new ValidationPipe({
     errorHttpStatusCode: HttpStatus.UNPROCESSABLE_ENTITY,
     transform:true
   }));
   app.enableCors();
-  const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
-  const port = process.env.PORT || 3002;
+  
   await app.listen(port);
+
+
   Logger.log(
     `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
   );
