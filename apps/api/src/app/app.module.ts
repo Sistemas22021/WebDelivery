f79a3a99a96121c03714040ad867bb67b9eb2e66
Sentineldev <figueraj196@gmail.com>
2023-07-01 14:44:16 -0400

    Eliminados archivos de app sin uso

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -1,15 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { DishModule } from './dish/dish.module';
 import { OrderModule } from './order/order.module';
 import appConfig from './config/app.config';
 
 @Module({
   imports: [DishModule, OrderModule,ConfigModule.forRoot( { load: [appConfig] } )],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
