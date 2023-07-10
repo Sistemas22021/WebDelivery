4c9089e5ddfff0a275f53ca85ba6f1a3b5534310
Sentineldev <figueraj196@gmail.com>
2023-07-09 22:49:24 -0400

    Agregado ajuste de layout a los platillos

--- apps/admin/src/app/components/OrderUpdateConfirmationModal.tsx
+++ apps/admin/src/app/components/OrderUpdateConfirmationModal.tsx
@@ -1,3 +1,4 @@
+import { OrderStatus } from "./pages/Orders/enums/OrderStatus.enum";
 import OrderInterface from "./pages/Orders/interfaces/order.interface";
 
 declare global {
@@ -18,21 +19,28 @@ export default function OrderUpdateConfirmationModal(props: {
     }
 
     return (
-        <>
-            {/* Open the modal using ID.showModal() method */}
-           
-            <dialog id="my_modal_1" className="modal">
-                <form method="dialog" className="modal-box">
-                    <p className="py-4 text-lg font-semibold">Esta seguro que desea cambiar el estado de la orden?</p>
-                    <div className="modal-action flex justify-start">
-                    {/* if there is a button in form, it will close the modal */}
-                        <div className="flex-1">
-                            <button onClick={onCloseHandler} className="btn btn-secondary">Cerrar</button>
-                        </div>
-                        <button onClick={onCloseHandler} className="btn btn-primary">Aceptar</button>
+        <dialog id="my_modal_1" className="modal">
+            <form method="dialog" className="modal-box">
+                {
+                    props.currentOrder
+                    ?
+                    <p className="py-4 text-lg ">Esta seguro que desea cambiar el estado de la orden a  
+                    {
+                        props.currentOrder?.status === OrderStatus.PENDING 
+                        ? <span className="font-semibold"> En Progreso</span>
+                        : <span className="font-semibold"> Completo</span>
+                    }
+                    ?
+                </p>
+                : ''
+                }
+                <div className="modal-action flex justify-start">
+                    <div className="flex-1">
+                        <button onClick={onCloseHandler} className="btn btn-secondary">Cerrar</button>
                     </div>
-                </form>
-            </dialog>
-        </>
+                    <button onClick={onCloseHandler} className="btn btn-primary">Aceptar</button>
+                </div>
+            </form>
+        </dialog>
     )
 }
--- apps/admin/src/app/components/pages/Orders/Order.tsx
+++ apps/admin/src/app/components/pages/Orders/Order.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { OrderStatus } from "./enums/OrderStatus.enum";
 import OrderInterface from "./interfaces/order.interface";
 
@@ -9,42 +10,44 @@ export default function Order(props:
     setCurrentOrder: React.Dispatch<React.SetStateAction<OrderInterface | null>>
 }) {
 
-    const element_type = props.order.status === OrderStatus.PENDING ? 
-    'warning' : 
-    'info';
-    const button_text = props.order.status === OrderStatus.PENDING ? 
-    "Atender"
-    : "Completar"
-    
+   
     function onClickHandler() {
         props.setCurrentOrder(props.order);
     }
 
     return(
-        <div className="border-gray-200 border-2 p-6 xl:w-3/4 ">
+        <div className="border-gray-200 border-2 p-6 xl:w-3/4 shadow-2xl rounded-md">
             
             <div className="flex flex-col  lg:flex-row gap-4">
-                <div className="flex-1 flex flex-row justify-center lg:flex-row lg:justify-start">
-                    <button onClick={onClickHandler} className={`btn btn-wide btn-${element_type}`}>{button_text}</button>
+                <div className="flex-1 flex flex-row lg:flex-row lg:justify-star">
+                    {
+                        props.order.status === OrderStatus.PENDING
+                        ? <button onClick={onClickHandler} className='btn btn-wide btn-warning'>Atender</button>
+                        : <button onClick={onClickHandler} className='btn btn-wide btn-info'>Completar</button>
+                    }
                 </div>
                 <div className="flex flex-row justify-center">
-                    <div className={`badge badge-${element_type} text-md p-3`}>{props.order.status}</div>
+                    {
+                        props.order.status === OrderStatus.PENDING
+                        ? <div className='badge badge-warning text-md p-3'>{props.order.status}</div>
+                        : <div className='badge badge-info text-md p-3'>{props.order.status}</div>
+                    }
                 </div>
             </div>
-            <div className="flex flex-col gap-6 py-4 w-3/4">
-                <div className="flex flex-col 2xl:flex-row gap-4 lg:gap-10">
+            <div className="flex flex-col gap-6 py-4 2xl:w-3/4">
+                <div className="grid grid-cols-1 sm:md:grid-cols-2  md:grid-cols-2 lg:grid-cols-2 gap-4">
                     <OrderField label="Nombre" field={props.order.client.name}/>
                     <OrderField label="Cedula" field={props.order.client.identification}/>
                     <OrderField label="Fecha y Hora" field={props.order.created_at}/>
-                    <OrderField label="Total a pagar" field={String(props.order.order_bill)}/>
+                    <OrderField label="Total a pagar" field={`${String(Number(props.order.order_bill).toFixed(2))} $`}/>
                     <OrderField label="Correo" field={props.order.client.email}/>
                 </div>
                 <div>
                     <OrderField label="Direccion" field={props.order.client.address}/>
                 </div>
                 <div>
-                    <h3 className="text-gray-500">Pedido</h3>
-                    <div className="flex flex-col lg:flex-row gap-4">
+                    <h3 className="text-gray-500 py-4">Pedido</h3>
+                    <div className="grid grid-cols-1 lg:grid-cols-2 gap-4">
                         {props.order.dishes.map((order_dish, index) => <OrderItem key={order_dish.dish.name+index} label={`${order_dish.dish.name} X ${order_dish.quantity}`} />)}
                     </div>
                 </div>
@@ -53,11 +56,12 @@ export default function Order(props:
     )
 }
 
+
 function OrderField(props: { label: string, field: string }){
     return(
         <div>
             <h3 className="text-gray-500">{props.label}</h3>
-            <h3>{props.field}</h3>
+            <h3 className="text-md lg:text-xl">{props.field}</h3>
         </div>
     );
 }
