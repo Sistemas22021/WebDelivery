21d9a145355975fde08add90426fbc2d980da58d
Sentineldev <84209942+Sentineldev@users.noreply.github.com>
2023-07-14 08:58:13 -0400

    Update app.tsx

--- apps/admin/src/app/app.tsx
+++ apps/admin/src/app/app.tsx
@@ -2,7 +2,6 @@ import Sidebar from './components/Sidebar/Sidebar';
 import OnSnapShotNotify from './components/pages/Orders/utils/onSnapShotNotify.util';
 
 
-console.log();
 OnSnapShotNotify();
 
 export function App() {
